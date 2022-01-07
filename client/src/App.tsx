bf206c6fcce7167c9dbb790679a9dac00fb4a991
SebaAlex <sebastiank24@wp.pl>
2022-01-07 09:58:23 +0100

    app change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import store from "./store/store";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+
 import Dashboard from './layout/Dashboard';
 
 function App() {
