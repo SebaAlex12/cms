1ff19f2fc9d5feb29aca1ca78d24128487ed8b3e
Seba <sebastian@blumo.pl>
2024-11-20 12:08:11 +0100

    upd

--- api/src/config/keys_prod.ts
+++ api/src/config/keys_prod.ts
@@ -1,9 +1,9 @@
 import { Iconfig } from "./interfaces";
 
 const config:Iconfig = {
-    DBhost: process.env.DB_HOST,
-    DBuser: process.env.DB_USER,
-    DBpassword: process.env.DB_PASSWORD,
-    DBname: process.env.DB_NAME
+    DBhost: process.env.DB_HOST || '',
+    DBuser: process.env.DB_USER || '',
+    DBpassword: process.env.DB_PASSWORD || '',
+    DBname: process.env.DB_NAME || ''
 }
 export default config;
